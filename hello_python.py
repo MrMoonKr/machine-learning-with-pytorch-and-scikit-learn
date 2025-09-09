32ab0184e34cc95fc97606bdf2d1e9734ce3d77d
Mr.Moon <MrMoonKr@gmail.com>
2025-09-09 15:27:51 +0900

    ipykernel 설치하여 VS Code 환경에서 주피터노트북 사용 환경을 추축합니다 ...

---
+++ hello_python.py
@@ -0,0 +1,13 @@
+# .venv 파이썬 가상환경 생성할것.
+# $ python -m venv .venv
+# $ ./.venv/Scripts/Activate.ps1
+# $ (.venv) pip --version
+# $ (.venv) pip list
+# $ (.venv) pip install ipykernel
+# $ (.venv) pip list
+# $ (.venv) pip freeze > requirements.txt
+
+
+print( 'Hello Python', 'with VS Code', 'DevOps 체크입니다' )
+print( 'ipykernel 설치하여 주피터노트북 사용환경을 구축합니다' )
+
